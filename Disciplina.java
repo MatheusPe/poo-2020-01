fdb0da6e4371b11db2beb59398a72dd9d366b6d6
MatheusPe <m4thperes@gmail.com>
2020-10-13 19:46:39 -0300

    Add files via upload

---
+++ Disciplina.java
@@ -0,0 +1,10 @@
+package hi;
+
+public class Disciplina {
+
+	public static void main(String[] args) {
+		System.out.println("POO");
+
+	}
+
+}
---
+++ Nome.java
@@ -0,0 +1,10 @@
+package hi;
+
+public class Nome {
+
+	public static void main(String[] args) {
+		System.out.println("Meu nome é Matheus.");
+
+	}
+
+}
